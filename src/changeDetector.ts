931c0332e6da5733de93c78c586a0f8fc001d275
Hamzah Haider (agent) <hamzahhaider1@gmail.com>
2025-09-27 21:40:11 -0400

    feat: update .ts files

--- src/changeDetector.ts
+++ src/changeDetector.ts
@@ -62,7 +62,7 @@ export class ChangeDetector {
 
         this.disposables.push(
             configChangeListener,
-            commandListener,
+            aiEventDetector,
             aiCompletionListener,
             cursorChangeListener
         );
